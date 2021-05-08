f235a29fa423a7472c3488d3ef0a3dbf5d1b662f
Marlon Luft Salvador <marlon-luft-salvador@hotmail.com>
2021-05-07 21:47:00 -0300

    Initial code

---
+++ HowMuchItCost/HowMuchItCost/Controllers/CryptoCurrencyController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HowMuchItCost.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CryptoCurrencyController : BaseController
+    {
+        [HttpGet("Doge")]
+        public IActionResult Doge()
+        {
+            decimal value = 20.59m;
+
+            return Ok(value);
+        }
+    }
+}
---
+++ HowMuchItCost/HowMuchItCost/Controllers/PingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HowMuchItCost.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PingController : BaseController
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("Pong");
+        }
+    }
+}
